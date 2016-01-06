807c0af3ebe25d2f632606b8b9a93f6d09ed7c98
Martin Heinrich <martin.heinrich.dresden@gmx.de>
2016-01-06 10:22:22 +0100

    fixed generic bug

--- src/com/abc/helloworld/task6/Main.java
+++ src/com/abc/helloworld/task6/Main.java
@@ -3,7 +3,7 @@ package com.abc.helloworld.task6;
 public class Main {
 
 	public static void main(String[] args) {
-		MyArrayList<String> myArrayList = new MyArrayList<String>();
+		MyArrayList myArrayList = new MyArrayList();
 		myArrayList.add("Hallo");
 		myArrayList.add("Welt");
 	}
